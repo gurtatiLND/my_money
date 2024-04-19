f9382d4e6ae6395caabc2a3a25bfbf0979dba54d
GurTati <tatiana.gurova.j@gmail.com>
2024-04-19 14:04:20 +0100

    create transactionList and queries

---
+++ src/hooks/useCollection.js
@@ -0,0 +1,38 @@
+import { useEffect, useState, useRef } from "react"
+import { projectFirestore } from "../firebase/config"
+
+export const useCollection = (collection, _query) => {
+    const [documents, setDocuments] = useState(null)
+    const [error, setError] = useState(null)
+
+    //if we don't use this --> infinite loop in useEffect
+    //-query is an array and is 'different' on every function call
+    const query = useRef(_query).current
+
+    useEffect(() => {
+        let ref = projectFirestore.collection(collection)
+
+        if (query) {
+            ref = ref.where(...query)
+        }
+
+        const unsubscribe = ref.onSnapshot((snapshot) => {
+            let results = []
+            snapshot.docs.forEach(doc => {
+                results.push({ ...doc.data(), id: doc.id })
+            })
+            //update state
+            setDocuments(results)
+            setError(null)
+        }, (error) => {
+            console.log(error)
+            setError('could not fetch the data')
+        })
+    
+    //unsubscribe on unmounts
+    return () => unsubscribe()
+
+    }, [collection, query]) 
+
+    return { documents, error }
+}
