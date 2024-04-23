8f989795e2e591709d5b4db1d8587f6222562379
GurTati <tatiana.gurova.j@gmail.com>
2024-04-23 13:53:12 +0100

    change name

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -12,7 +12,7 @@ export default function Navbar() {
   return (
     <nav className={styles.navbar}>
         <ul>
-            <li className={styles.title}>MyMoney</li>
+            <li className={styles.title}>All My Money</li>
             
             {!user && (
               <>
